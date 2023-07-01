69b237d481652eb0c0c328be6413903ce7e22745
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2023-07-01 12:41:13 +0200

    domain data finished

--- Fsia.Tennis/Tennis.fs
+++ Fsia.Tennis/Tennis.fs
@@ -1,1 +1,74 @@
 ﻿module Fsia.Tennis
+
+type Player =
+    | PlayerOne
+    | PlayerTwo
+
+let otherPlayer player =
+    match player with
+    | PlayerOne -> PlayerTwo
+    | PlayerTwo -> PlayerOne
+
+module Game =
+    module Point =
+        type Point =
+            | Love
+            | Fifteen
+            | Thirty
+
+        // How would you do this in C#?
+        let increment point =
+            match point with
+            | Love -> Some Fifteen
+            | Fifteen -> Some Thirty
+            | Thirty -> None
+
+    open Point
+
+    type PointsScore = {
+        PlayerOne: Point
+        PlayerTwo: Point
+    }
+
+    module PointsScore =
+        let pointForPlayer pointsScore player =
+            match player with
+            | PlayerOne -> pointsScore.PlayerOne
+            | PlayerTwo -> pointsScore.PlayerTwo
+
+        let updatePlayerPoint pointsScore player point =
+            match player with
+            | PlayerOne -> {
+                pointsScore with
+                    PlayerOne = point
+              }
+            | PlayerTwo -> {
+                pointsScore with
+                    PlayerTwo = point
+              }
+
+    type GamePointScore = {
+        PlayerWithGamePoint: Player
+        OtherPlayerPoint: Point
+    }
+
+    type GameWonOtherPoint =
+        | Point of Point
+        | Forty
+
+    type GameScore = {
+        PlayerThatWon: Player
+        OtherPlayerPoint: GameWonOtherPoint
+    }
+
+    type Score =
+        | Points of PointsScore
+        | GamePoint of GamePointScore
+        | Deuce
+        | Advantage of Player
+        | Game of GameScore
+
+    let newGame = Points {
+        PlayerOne = Love
+        PlayerTwo = Love
+    }
